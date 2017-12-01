2897112e43495edb1975ab09b1dbeedfff63bced
Robert Ciliberto <rciliberto2@bostonk12.org>
2017-12-01 13:06:08 -0500

    Add loop for cmd like structure

--- TeacherTool.py
+++ TeacherTool.py
@@ -8,10 +8,10 @@ def choice_creator():
     multiple_choice = "abcdefg"
 
     # ask for number of questions to be filled out
-    number_questions = int(input("How many multiple choice questions do you want? "))
+    number_questions = int(input("How many multiple choice questions do you want?"))
 
     # ask for the number of answers per question
-    choices_per_question = int(input("How many choices per question? (Max 7) "))
+    choices_per_question = int(input("How many choices per question? (Max 7)\n>"))
     test = ""
 
     for i in range(number_questions):
@@ -20,24 +20,31 @@ def choice_creator():
 
         for j in range(choices_per_question):
             possible_answer = input("Type a possible answer.\n>")
-            test += "  " + multiple_choice[j+1] + "." + possible_answer + "\n"
+            test += "  " + multiple_choice[j] + "." + possible_answer + "\n"
 
     print(test)
 
-##########
-## Main ##
-##########
+"""
+MAIN
+"""
 
-# introduce the program and ask for user input
+# introduce the program
 print("Hello! Welcome to TeacherTool. This is your one stop shop for all your teaching needs.\n"\
 "type \"help\" for directions and possible commands.")
 
-USER_CHOICE = str(input(">"))
+"""Begin inputs"""
+while True:
+    USER_CHOICE = str(input(">"))
 
-# possible actions user can do
-if USER_CHOICE == "help":
-    print("List of possible commands: \n"\
-    "To create a multiple choice test, type in \"Create multiple choice test\"\n")
+    if USER_CHOICE == "exit":
+        break
 
-if USER_CHOICE == "Create multiple choice test":
-    choice_creator()
+    elif USER_CHOICE == "help":
+        print("List of possible commands: \n"\
+        "To create a multiple choice test, type in \"Create multiple choice test\"\n")
+
+    elif USER_CHOICE == "Create multiple choice test":
+        choice_creator()
+
+    else:
+        print("Unknown command. type \"help\" for a list of commands")
