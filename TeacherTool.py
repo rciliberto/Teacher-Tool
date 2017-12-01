60d22f085c598f0b9d7d964a9342bc9af41ec63b
David <chendavid2012@gmail.com>
2017-11-30 23:28:32 -0500

    Fix ordering issues
    - Make choiceCreator actually work

--- TeacherTool.py
+++ TeacherTool.py
@@ -1,18 +1,40 @@
-print("Hello! Welcome to TeacherTool. This is your one stop shop for ")
-
-def choiceCreator:
-
-    numberquestions = int(input("How many multiple choice questions do you want?"))
+# this creates a multiple choice test.
+def choiceCreator():
+    # storage string for response choices
     multiple_choice = "abcdefg"
-    numbermultiplechoice = int(input("How many choices per question? (Max 7)"))
+
+
+    numberquestions = int(input("How many multiple choice questions do you want? "))
+
+    numbermultiplechoice = int(input("How many choices per question? (Max 7) "))
     iterator = 1
     finalstring = ""
 
+    # this while loop cycles through for each question.
     while (iterator != numberquestions + 1):
-        iterator += 1
-        question = input("What is your first question?")
+        question = input("What is question number " + str(iterator) + "?")
+        finalstring += str(iterator) + ". " + question + "\n"
         choicecounter = 0
 
-        while (choicecounter < len(multiple_choice)):
-            choice = multiple_choice[choicecounter]
+        # this nested while loop cycles through the possible choices for the question it applies to.
+        while (choicecounter < numbermultiplechoice):
+            possibleAnswer = input("Type a possible answer.")
+            finalstring += multiple_choice[choicecounter] + "." + possibleAnswer + "\n"
             choicecounter += 1
+
+        iterator += 1
+
+    print(finalstring)
+
+# intro
+print("Hello! Welcome to TeacherTool. This is your one stop shop for (type in \"help\" for directions and possible commands).")
+user_choice = input("What do you want to do?\n")
+
+# possible actions user can do
+if (user_choice) == "help":
+    print("List of possible commands: \nTo create a multiple choice test, type in \"Create multiple choice test\"\nTo create ")
+
+if (user_choice) == "Create multiple choice test":
+    choiceCreator()
+
+testAnswers = []
