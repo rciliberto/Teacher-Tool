fc6241bb38fe516f201ca0eb3d0623f748af2ac8
Robert Ciliberto <rciliberto2@bostonk12.org>
2017-12-01 01:23:21 -0500

    Update formatting
    Code now looks more Pythony than before and test output looks more like a test.

--- TeacherTool.py
+++ TeacherTool.py
@@ -1,22 +1,26 @@
-# this creates a multiple choice test.
-def choiceCreator():
+"""
+Teacher Tool is a suite of useful tools for teachers.
+These include a test generator at the moment.
+"""
+def choice_creator():
+    """Ask for questions & answers and print the generated test"""
     # storage string for response choices
     multiple_choice = "abcdefg"
 
     # ask for number of questions to be filled out
-    questionNum = int(input("How many multiple choice questions do you want? "))
+    number_questions = int(input("How many multiple choice questions do you want? "))
 
     # ask for the number of answers per question
-    choicesPerQuestion = int(input("How many choices per question? (Max 7) "))
+    choices_per_question = int(input("How many choices per question? (Max 7) "))
     test = ""
 
-    for i in range(questionNum):
-        question = input("What is question number " + str(i+1) + "?")
+    for i in range(number_questions):
+        question = input("What is question number " + str(i+1) + "?\n>")
         test += str(i+1) + ". " + question + "\n"
 
-        for j in range(choicesPerQuestion):
-            possibleAnswer = input("Type a possible answer.")
-            test += multiple_choice[j+1] + "." + possibleAnswer + "\n"
+        for j in range(choices_per_question):
+            possible_answer = input("Type a possible answer.\n>")
+            test += "  " + multiple_choice[j+1] + "." + possible_answer + "\n"
 
     print(test)
 
@@ -25,12 +29,15 @@ def choiceCreator():
 ##########
 
 # introduce the program and ask for user input
-print("Hello! Welcome to TeacherTool. This is your one stop shop for all your teaching needs.\ntype \"help\" for directions and possible commands.")
-user_choice = input("What do you want to do?\n")
+print("Hello! Welcome to TeacherTool. This is your one stop shop for all your teaching needs.\n"\
+"type \"help\" for directions and possible commands.")
+
+USER_CHOICE = str(input(">"))
 
 # possible actions user can do
-if user_choice == "help":
-    print("List of possible commands: \nTo create a multiple choice test, type in \"Create multiple choice test\"\n")
+if USER_CHOICE == "help":
+    print("List of possible commands: \n"\
+    "To create a multiple choice test, type in \"Create multiple choice test\"\n")
 
-if user_choice == "Create multiple choice test":
-    choiceCreator()
+if USER_CHOICE == "Create multiple choice test":
+    choice_creator()
