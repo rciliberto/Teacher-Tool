e25fa740436b958425f0408b69bc20a43ce33fa6
Robert Ciliberto <rciliberto2@bostonk12.org>
2017-12-01 23:44:05 -0500

    Change console format
    changed the format of i/o to be more visually pleasing

--- TeacherTool.py
+++ TeacherTool.py
@@ -8,21 +8,21 @@ def choice_creator():
     multiple_choice = "abcdefg"
 
     # ask for number of questions to be filled out
-    number_questions = int(input("How many multiple choice questions do you want?"))
+    number_questions = int(input("\nHow many multiple choice questions do you want?"))
 
     # ask for the number of answers per question
-    choices_per_question = int(input("How many choices per question? (Max 7)\n>"))
+    choices_per_question = int(input("\nHow many choices per question? (Max 7)\n>"))
     test = ""
 
     for i in range(number_questions):
-        question = input("What is question number " + str(i+1) + "?\n>")
+        question = input("\nWhat is question number " + str(i+1) + "?\n>")
         test += str(i+1) + ". " + question + "\n"
 
         for j in range(choices_per_question):
-            possible_answer = input("Type a possible answer.\n>")
+            possible_answer = input("\nType a possible answer.\n>")
             test += "  " + multiple_choice[j] + "." + possible_answer + "\n"
 
-    print(test)
+    print("\n" + test)
 
 """
 MAIN
@@ -32,18 +32,21 @@ MAIN
 print("Hello! Welcome to TeacherTool. This is your one stop shop for all your teaching needs.\n"\
 "type \"help\" for directions and possible commands.")
 
-"""Begin inputs"""
+#Begin inputs
 while True:
     USER_CHOICE = str(input(">"))
 
-    if USER_CHOICE == "exit":
+    if USER_CHOICE.upper() == "EXIT":
         break
 
-    elif USER_CHOICE == "help":
-        print("List of possible commands: \n"\
-        "To create a multiple choice test, type in \"Create multiple choice test\"\n")
+    elif USER_CHOICE.upper() == "HELP":
+        print("\nList of possible commands: \n"\
+        "EXIT       -   Exit TeacherTool\n"\
+        "HELP       -   Present this list of commands\n"\
+        "\n"\
+        "MKMCTEST   -   Make a multiple choice test\"\n")
 
-    elif USER_CHOICE == "Create multiple choice test":
+    elif USER_CHOICE.upper() == "MKMCTEST":
         choice_creator()
 
     else:
