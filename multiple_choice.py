6050d3d0fd50f815110fb59ca846e32ed6e4c8a2
Robert Ciliberto <rciliberto2@bostonk12.org>
2017-12-09 14:50:23 -0500

    Include classes

--- multiple_choice.py
+++ multiple_choice.py
@@ -1,19 +1,20 @@
 """
 Includes multiple choice test and question classes and related final static variables
 """
-LETTERS = "abcdefg"
 
-class MCQuestion:
+class MCQuestion(object):
     """A question object that stores a question and its answers"""
+    LETTERS = "abcdefg"
+    
+    def __init__(self):
+        self.question = ""
+        self.answers = []
+        self.correct_index = -1
 
-    question = ""
-    answers = []
-    correct_index = -1
-
-    def __repr__(self):
-        output = self.question + "\n"
-        for i in self.answers:
-            output += LETTERS[i] + ". " + self.answers[i] + "\n"
+    def __str__(self):
+        output = "  " + self.question + "\n"
+        for i in range(len(self.answers)):
+            output += "     " + MCQuestion.LETTERS[i] + ". " + self.answers[i] + "\n"
         return output
 
     def set_question(self, question):
@@ -26,15 +27,18 @@ class MCQuestion:
         if is_correct:
             self.correct_index = len(self.answers)-1
 
-class MCTest:
+class MCTest(object):
     """A test object that stores multiple questions"""
 
-    test_name = ""
-    questions = []
+    def __init__(self):
+        self.test_name = ""
+        self.questions = []
 
-    def __repr__(self):
+    def __str__(self):
+        output = self.test_name + "\n"
         for question in self.questions:
-            print(question + "\n")
+            output += str(question) + "\n"
+        return output
 
     def add_question(self, question):
         """add a question to the test"""
